991d5c4e2aa907c84806eed0ca3b3637231e8515
Angelo Pirola <angelo9871@gmail.com>
2024-04-11 12:15:48 +0200

    Refactoring WebApp

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using IdentityManagerUI.Models;
-using Microsoft.AspNetCore.Identity;
+﻿using IdentityManagerUI.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,9 +15,23 @@ namespace WebApplication1.Data
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<ApplicationUser>().HasMany(p => p.Roles).WithOne().HasForeignKey(p => p.UserId).IsRequired().OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<ApplicationUser>().HasMany(e => e.Claims).WithOne().HasForeignKey(e => e.UserId).IsRequired().OnDelete(DeleteBehavior.Cascade);
-            builder.Entity<ApplicationRole>().HasMany(r => r.Claims).WithOne().HasForeignKey(r => r.RoleId).IsRequired().OnDelete(DeleteBehavior.Cascade);
+            builder.Entity<ApplicationUser>()
+                .HasMany(p => p.Roles)
+                .WithOne()
+                .HasForeignKey(p => p.UserId).IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ApplicationUser>()
+                .HasMany(e => e.Claims)
+                .WithOne()
+                .HasForeignKey(e => e.UserId).IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ApplicationRole>()
+                .HasMany(r => r.Claims)
+                .WithOne()
+                .HasForeignKey(r => r.RoleId).IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IdentityManagerUI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using WebApplication1.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using FileContextCore;
-using IdentityManagerUI.Models;
 using System.Security.Claims;
+using WebApplication1.Data;
 
 namespace WebApplication1
 {
@@ -25,57 +19,55 @@ namespace WebApplication1
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseFileContextDatabase());
+            var connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
 
             services.AddDefaultIdentity<ApplicationUser>()
                 .AddRoles<ApplicationRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                //.AddDefaultTokenProviders()
+                //.AddClaimsPrincipalFactory<CustomClaimsPrincipalFactory>()
+                //.AddPasswordValidator<CommonPasswordValidator<ApplicationUser>>()
+                ;
 
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                //app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
             //Temporary middleware for testing, don't use in production!
             app.Use(async (context, next) =>
             {
-                var userIdentity = new ClaimsIdentity(new[] {
-                    new Claim(ClaimTypes.Role, "Admin")
-                }, "Demo");
+                var userIdentity = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Role, "Admin") }, "Demo");
                 context.User = new ClaimsPrincipal(userIdentity);
                 await next.Invoke();
             });
-            //app.UseAuthentication();
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "areas",
-                    pattern: "{area}/{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(name: "areas", pattern: "{area}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
